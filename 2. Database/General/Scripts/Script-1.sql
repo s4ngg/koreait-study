ff7bbe00874a87beced7f71136fd85176127ebf5
김상우 <s4ngg@naver.com>
2026-01-22 18:17:24 +0900

    mySQL

--- 2. Database/General/Scripts/Script-1.sql
+++ 2. Database/General/Scripts/Script-1.sql
@@ -118,6 +118,70 @@ CREATE TABLE test_member (
  SELECT * FROM free_board
  
  
+ CREATE TABLE ddl_alter_test (
+ 	alter_id varchar(255),
+ 	alter_name varchar(255),
+ 	alter_as varchar(255)
+ );
+ 
+ -- 테이블 이름 변경
+ ALTER TABLE ddl_alter_test RENAME TO ddl_alter;
 
+ -- 테이블 구조 변경 (컬럼 변경)
+ ALTER TABLE ddl_alter
+ MODIFY alter_as varchar(100); -- modify는 컬럼의 속성 변경하는 명령어
+ 
+ -- 테이블 구조 변경 (컬럼 추가)
+ ALTER TABLE ddl_alter 
+ADD alter_pwd varchar(255); -- add는 컬럼을 추가하는 명령어
 
+-- 테이블 구조 변경 (컬럼 삭제)
+ALTER TABLE ddl_alter 
+DROP COLUMN alter_as; 
+-- drop column은 컬럼을 삭제하는 명령어 / drop은 종류가 여러가지라 삭제할 속성을 추가 작성해줘야함
+-- rename 도 마찬가지로 rename column사용
+ 
+-- alter_name이라는 컬럼에 중복된 값이 들어가서는 안된다라는 조건을 추가
+-- constraint : 제약조건 추가
+-- unique : 중복 불가
+ALTER TABLE ddl_alter 
+ADD CONSTRAINT alter_name_unique UNIQUE(alter_name); -- 제약의 이름설정
+ 
+-- "sang", "김상우", 'qwer1234!'
+-- "test", "김상우", '1234'
+INSERT INTO ddl_alter 
+VALUES ("sang", "김상우66", 'qwer1234!'),
+	   ("test", "김상우55", '1234');
 
+SELECT * FROM ddl_alter;
+
+-- MySQL에서는 지원 X, 수정하려면 제거 후 다시 추가
+-- ALTER TABLE ddl_alter 
+-- RENAME CONSTRAINT alter_name_unique TO name_unique; 
+
+ALTER TABLE ddl_alter
+DROP CONSTRAINT alter_name_unique;
+
+ALTER TABLE ddl_alter
+ADD CONSTRAINT name_unique UNIQUE(alter_name);
+
+CREATE TABLE test_employees(
+	te_employee_id int,
+	te_first_name varchar(50),
+	te_last_name varchar(50),
+	te_salary int,
+	te_department varchar(50)
+);
+-- Q1. 실습
+ALTER TABLE test_employees RENAME TO STAFF;
+ALTER TABLE STAFF 
+MODIFY te_department varchar(100),
+RENAME COLUMN te_last_name TO surname,
+DROP COLUMN te_first_name,
+ADD te_hire_date datetime,
+ADD CONSTRAINT te_salary_check UNIQUE(te_salary);
+
+DROP TABLE ddl_alter;
+
+-- free_board, staff, test_member 제거하기
+DROP TABLE free_board, staff, test_member;
